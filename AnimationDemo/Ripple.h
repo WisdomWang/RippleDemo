06d6f1f1786ff5affd0aecef280c4d770bb32969
WisdomWang <294814110@qq.com>
2018-11-28 16:38:06 +0800

    first commit

---
+++ AnimationDemo/Ripple.h
@@ -0,0 +1,13 @@
+//
+//  Ripple.h
+//  AnimationDemo
+//
+//  Created by Cary on 2018/9/3.
+//  Copyright © 2018年 Cary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Ripple : UIView
+
+@end
---
+++ AnimationDemo/Ripple.m
@@ -0,0 +1,21 @@
+//
+//  Ripple.m
+//  AnimationDemo
+//
+//  Created by Cary on 2018/9/3.
+//  Copyright © 2018年 Cary. All rights reserved.
+//
+
+#import "Ripple.h"
+
+@implementation Ripple
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
